a87e24e95358854aa8c11f0ae1b2aa3acfcff9e9
ashq7 <32597619+ashq7@users.noreply.github.com>
2017-10-20 15:48:13 -0500

    Removing setMaxSpeed
    Commenting out setMaxSpeed since it was removed from the SDK

--- TeamCode/src/main/java/org/firstinspires/ftc/Tempest_2017_2018/teamcode/DriveTrains/HolonomicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/Tempest_2017_2018/teamcode/DriveTrains/HolonomicDrive.java
@@ -41,25 +41,25 @@ public class    HolonomicDrive {
         NW.setMode(encMode);
         NW.setDirection(DcMotor.Direction.REVERSE);
         NW.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        NW.setMaxSpeed(speed);
+        //NW.setMaxSpeed(speed);
 
         NE = HWMap.dcMotor.get("NE");
         NE.setMode(encMode);
         NE.setDirection(DcMotor.Direction.FORWARD);
         NE.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        NE.setMaxSpeed(speed);
+        //NE.setMaxSpeed(speed);
 
         SW = HWMap.dcMotor.get("SW");
         SW.setMode(encMode);
         SW.setDirection(DcMotor.Direction.REVERSE);
         SW.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        SW.setMaxSpeed(speed);
+        //SW.setMaxSpeed(speed);
 
         SE = HWMap.dcMotor.get("SE");
         SE.setMode(encMode);
         SE.setDirection(DcMotor.Direction.FORWARD);
         SE.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        SE.setMaxSpeed(speed);
+        //SE.setMaxSpeed(speed);
 
         gyro = new GyroScope();
         gyro.init(HWMap);
--- TeamCode/src/main/java/org/firstinspires/ftc/Tempest_2017_2018/teamcode/DriveTrains/SuperBasicHolonomicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/Tempest_2017_2018/teamcode/DriveTrains/SuperBasicHolonomicDrive.java
@@ -33,25 +33,25 @@ public class SuperBasicHolonomicDrive {
         NW.setMode(encMode);
         NW.setDirection(DcMotor.Direction.REVERSE);
         NW.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        NW.setMaxSpeed(speed);
+        //NW.setMaxSpeed(speed);
 
         NE = HWMap.dcMotor.get("NE");
         NE.setMode(encMode);
         NE.setDirection(DcMotor.Direction.FORWARD);
         NE.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        NE.setMaxSpeed(speed);
+        //NE.setMaxSpeed(speed);
 
         SW = HWMap.dcMotor.get("SW");
         SW.setMode(encMode);
         SW.setDirection(DcMotor.Direction.REVERSE);
         SW.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        SW.setMaxSpeed(speed);
+        //SW.setMaxSpeed(speed);
 
         SE = HWMap.dcMotor.get("SE");
         SE.setMode(encMode);
         SE.setDirection(DcMotor.Direction.FORWARD);
         SE.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        SE.setMaxSpeed(speed);
+        //SE.setMaxSpeed(speed);
     }
     public void pan(double theta, double power){
         if (power <= 1 && power >= -1){

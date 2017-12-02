b9773b7ab50d6e63947bbd3dafa1b6558ec9eb03
MollyfromTempest <molly.burke@gmail.com>
2017-12-02 12:17:27 -0600

    New Architechture in Jewel Autonomus
    Incorporated the new structure into the Jewel Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/Tempest_2017_2018/teamcode/Programs/Jewel_Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/Tempest_2017_2018/teamcode/Programs/Jewel_Autonomous.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.Tempest_2017_2018.teamcode.DriveTrains.HolonomicDrive;
 import org.firstinspires.ftc.Tempest_2017_2018.teamcode.Manipulators.Jewel_Arm;
+import org.firstinspires.ftc.Tempest_2017_2018.teamcode.Robot2017_2018;
 import org.firstinspires.ftc.Tempest_2017_2018.teamcode.Sensors.ColorSensorClass;
 
 /**
@@ -14,7 +15,7 @@ import org.firstinspires.ftc.Tempest_2017_2018.teamcode.Sensors.ColorSensorClass
  */
 @Autonomous
 public class Jewel_Autonomous extends LinearOpMode{
-    HolonomicDrive Holodrive;
+    Robot2017_2018 TempestRobot;
 
     public void Sleep(long ticks) throws InterruptedException {
         long timer = System.currentTimeMillis();
@@ -25,23 +26,23 @@ public class Jewel_Autonomous extends LinearOpMode{
 
     @Override
     public void runOpMode()throws InterruptedException{
-        Holodrive = new HolonomicDrive();
-        Holodrive.init(hardwareMap);
+        TempestRobot = new Robot2017_2018();
+        TempestRobot.init(hardwareMap);
         waitForStart();
         //Turn on the color sensor LEDS
-        Holodrive.color.leftColor.enableLed(true);
-        Holodrive.color.rightColor.enableLed(true);
+        TempestRobot.color.leftColor.enableLed(true);
+        TempestRobot.color.rightColor.enableLed(true);
         //Lower the jewel arm -- necessary regardless of color
-        Holodrive.jewelArm.jewelArmDown();
+        TempestRobot.jewelArm.jewelArmDown();
         //Wait for two seconds so that it has time to lower
         Sleep(2000);
-        boolean rightBlue = Holodrive.color.isBlue(Holodrive.color.rightColor); //right color sensor is blue
-        boolean rightRed = Holodrive.color.isRed(Holodrive.color.rightColor); //right color sensor is red
-        boolean leftBlue = Holodrive.color.isBlue(Holodrive.color.leftColor); //left color sensor is blue
-        boolean leftRed = Holodrive.color.isRed(Holodrive.color.leftColor); //left color sensor is red
+        boolean rightBlue = TempestRobot.color.isBlue(TempestRobot.color.rightColor); //right color sensor is blue
+        boolean rightRed = TempestRobot.color.isRed(TempestRobot.color.rightColor); //right color sensor is red
+        boolean leftBlue = TempestRobot.color.isBlue(TempestRobot.color.leftColor); //left color sensor is blue
+        boolean leftRed = TempestRobot.color.isRed(TempestRobot.color.leftColor); //left color sensor is red
 
 
-        if (Holodrive.BlueSwitch.getState()) {
+        if (TempestRobot.BlueSwitch.getState()) {
             //RED RED RED
             telemetry.addData("Right blue", rightBlue);
             telemetry.addData("Right red", rightRed);
@@ -61,31 +62,31 @@ public class Jewel_Autonomous extends LinearOpMode{
             } else if (leftBlue && !rightBlue) {
                 // Left blue and right not specified (but not also blue). Since we are red, we want to turn left.
                 telemetry.addData("Color sensor", "Left blue, right red or unspecified");
-                Holodrive.turnleftunlim(0.2);
+                TempestRobot.holoDrive.turnleftunlim(0.2);
                 Sleep(500);
-                Holodrive.stopmotors();
-                Holodrive.jewelArm.jewelArmUp();
+                TempestRobot.holoDrive.stopmotors();
+                TempestRobot.jewelArm.jewelArmUp();
             } else if (leftRed && !rightRed) {
                 // Left red and right not specified (but not also red). Since we are red, we want to turn right.
                 telemetry.addData("Color sensor", "Left red, right blue or unspecified");
-                Holodrive.turnrightunlim(0.2);
+                TempestRobot.holoDrive.turnrightunlim(0.2);
                 Sleep(500);
-                Holodrive.stopmotors();
-                Holodrive.jewelArm.jewelArmUp();
+                TempestRobot.holoDrive.stopmotors();
+                TempestRobot.jewelArm.jewelArmUp();
             }else if(!leftBlue && rightBlue){
                 //Right blue and left not specified (but not also blue). Since we are red, we want to turn right.
                 telemetry.addData("Color sensor", "Left red or unspecified, right blue");
-                Holodrive.turnrightunlim(0.2);
+                TempestRobot.holoDrive.turnrightunlim(0.2);
                 Sleep(500);
-                Holodrive.stopmotors();
-                Holodrive.jewelArm.jewelArmUp();
+                TempestRobot.holoDrive.stopmotors();
+                TempestRobot.jewelArm.jewelArmUp();
             }else if (!leftRed && rightRed){
                 //Right red and left not specified (but not also red). Since we are red, we want to turn left.
                 telemetry.addData("Color sensor", "Left blue or unspecified, right red");
-                Holodrive.turnleftunlim(0.2);
+                TempestRobot.holoDrive.turnleftunlim(0.2);
                 Sleep(500);
-                Holodrive.stopmotors();
-                Holodrive.jewelArm.jewelArmUp();
+                TempestRobot.holoDrive.stopmotors();
+                TempestRobot.jewelArm.jewelArmUp();
             }else {
                 //No reading? Ambiguous reading? Either way, something is wrong and we don't want to risk turning.
                 telemetry.addData("Color sensor", "No reading");
@@ -112,31 +113,31 @@ public class Jewel_Autonomous extends LinearOpMode{
             } else if (leftBlue && !rightBlue){
                 // Left blue and right not specified (but not also blue). Since we are blue, we want to turn right.
                 telemetry.addData ("Color sensor", "Left blue, right red or unspecified");
-                Holodrive.turnrightunlim(0.2);
+                TempestRobot.holoDrive.turnrightunlim(0.2);
                 Sleep(500);
-                Holodrive.stopmotors();
-                Holodrive.jewelArm.jewelArmUp();
+                TempestRobot.holoDrive.stopmotors();
+                TempestRobot.jewelArm.jewelArmUp();
             } else if (leftRed && !rightRed){
                 // Left red and right unspecified (but not also red). Since we are blue, we want to turn left.
                 telemetry.addData ("Color sensor", "Left red, right blue or unspecified");
-                Holodrive.turnleftunlim(0.2);
+                TempestRobot.holoDrive.turnleftunlim(0.2);
                 Sleep(500);
-                Holodrive.stopmotors();
-                Holodrive.jewelArm.jewelArmUp();
+                TempestRobot.holoDrive.stopmotors();
+                TempestRobot.jewelArm.jewelArmUp();
             } else if (!leftBlue && rightBlue){
                 //Right blue and left unspecified (but not also blue). Since we are blue, we want to turn left.
                 telemetry.addData ("Color sensor", "Left red or unspecified, right blue");
-                Holodrive.turnleftunlim(0.2);
+                TempestRobot.holoDrive.turnleftunlim(0.2);
                 Sleep(500);
-                Holodrive.stopmotors();
-                Holodrive.jewelArm.jewelArmUp();
+                TempestRobot.holoDrive.stopmotors();
+                TempestRobot.jewelArm.jewelArmUp();
             }else if (!leftRed && rightRed){
                 //Right red and left unspecified (but not also red). Since we are blue, we want to turn right.
                 telemetry.addData ("Color sensor", "Left blue or unspecified, right red");
-                Holodrive.turnrightunlim(0.2);
+                TempestRobot.holoDrive.turnrightunlim(0.2);
                 Sleep(500);
-                Holodrive.stopmotors();
-                Holodrive.jewelArm.jewelArmUp();
+                TempestRobot.holoDrive.stopmotors();
+                TempestRobot.jewelArm.jewelArmUp();
             }else {
                 //No reading? Ambiguous reading? Either way, something is wrong and we don't want to risk turning.
                 telemetry.addData("Color sensor", "No reading");

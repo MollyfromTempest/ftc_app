11f089a2e9c5007f4d986d31a02bee0e4c1e21c1
Aaron Cook <aarongregorycook@gmail.com>
2017-12-09 13:21:17 -0600

    Adding navx as a dependency

--- TeamCode/src/main/java/org/firstinspires/ftc/Tempest_2017_2018/teamcode/Sensors/NavX.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/Tempest_2017_2018/teamcode/Sensors/NavX.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.Tempest_2017_2018.teamcode.Sensors;
 
 /**
- * Created by Molly on 12/2/2017.
+ * Created by Molly
+ *
+ * on 12/2/2017.
  */
 
 import com.kauailabs.navx.ftc.AHRS;

3a9e55b8c24761822ea6725268ee361331d112d8
Aaron Cook <aarongregorycook@gmail.com>
2018-01-20 10:42:38 -0600

    Quick Run to Pos Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/Tempest_2017_2018/teamcode/Programs/Run_to_Position_Test.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.Tempest_2017_2018.teamcode.Programs;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.Tempest_2017_2018.teamcode.Robot2017_2018;
+
+/**
+ * Created by Aaron on 1/20/2018.
+ */
+
+@Autonomous
+public class Run_to_Position_Test extends LinearOpMode {
+    Robot2017_2018 Robot;
+
+    public Run_to_Position_Test(){}
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Robot.init(hardwareMap);
+        waitForStart();
+
+        Robot.glyphArm.liftArm.setTargetPosition(1000);
+
+        while(Robot.glyphArm.liftArm.isBusy()){idle();}
+    }
+}

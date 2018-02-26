678b042d5b4925fc7b1814291d6193027d51d044
ashq7 <aislinndream918@gmail.com>
2018-02-25 22:22:01 -0600

    Testing programs for unit circel and vuforia
    Both programs work (note that pi then pi over two is actually pi over 4 then pi over two). The vuforia program reads the vu mark and then moves left, right, or backwards, if the vumark is left, right, or center, respectively.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/Tempest_2017_2018/teamcode/Programs/PiThenPiOverTwo.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.Tempest_2017_2018.teamcode.Programs;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.Tempest_2017_2018.teamcode.Robot2017_2018;
+
+/**
+ * Created by AshQuinn on 2/24/18.
+ */
+
+@Autonomous
+public class PiThenPiOverTwo extends LinearOpMode {
+    Robot2017_2018 Robot;
+
+    public void Sleep(long ticks) throws InterruptedException {
+        long timer = System.currentTimeMillis();
+        while (System.currentTimeMillis() - timer < ticks) {
+            idle();
+        }
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Robot = new Robot2017_2018();
+        Robot.init(hardwareMap);
+        Robot.holoDrive.pan(Math.PI/4, 0.3);
+        Sleep(2000);
+        Robot.holoDrive.stopmotors();
+        Robot.holoDrive.pan(Math.PI/2, 0.3);
+        Sleep(2000);
+        Robot.holoDrive.stopmotors();
+    }
+}
